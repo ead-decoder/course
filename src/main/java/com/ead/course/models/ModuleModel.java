498ccc0d3cbb4e0c7ca58299658e4ce498921e3c
vinigomes <vinigomes@outlook.com>
2021-11-23 20:44:06 -0300

    created the course, module and lesson entities. In addition to enums, repositories and services

---
+++ src/main/java/com/ead/course/models/ModuleModel.java
@@ -0,0 +1,29 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Entity
+@Table(name = "TB_MODULES")
+public class ModuleModel {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID moduleId;
+    @Column(nullable = false, length = 150)
+    private String title;
+    @Column(nullable = false, length = 250)
+    private String description;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+}
