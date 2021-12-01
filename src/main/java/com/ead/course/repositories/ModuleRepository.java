657f29ca791969a397e6b6fc395aa20ab6558a94
vinigomes <vinigomes@outlook.com>
2021-12-01 18:37:23 -0300

    implmented delete methods on cascade with transactional

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -2,7 +2,6 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
