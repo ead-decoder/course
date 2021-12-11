fd3e912f88677c513d7badac22e15c205f2dbb10
vinigomes <vinigomes@outlook.com>
2021-12-11 11:09:51 -0300

    implemented course CRUD

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CourseServiceImpl implements CourseService {
@@ -39,5 +41,20 @@ public class CourseServiceImpl implements CourseService {
             }
         }
         courseRepository.delete(courseModel);
+    }
+
+    @Override
+    public CourseModel save(CourseModel courseModel) {
+        return courseRepository.save(courseModel);
+    }
+
+    @Override
+    public Optional<CourseModel> findById(UUID courseId) {
+        return courseRepository.findById(courseId);
+    }
+
+    @Override
+    public List<CourseModel> findAll() {
+        return courseRepository.findAll();
     }
 }
